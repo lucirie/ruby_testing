2a0bf61e746efcb60efbc5bed03a6a7b93e22564
Ziad Alezzi <lucirieko@gmail.com>
2025-11-09 11:02:49 +0200

    Solved exercise 10

--- spec/10_drink_spec.rb
+++ spec/10_drink_spec.rb
@@ -91,13 +91,16 @@ describe Drink do
     context 'when type is specified and ounces is default' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type.
+      subject(:preferred_drink) { described_class.new(type='coffee', 16) }
 
       # remove the 'x' before running this test
-      xit 'is your choice of beverage' do
+      it 'is your choice of beverage' do
+        expect(preferred_drink.type).to eq('coffee')
       end
 
       # remove the 'x' before running this test
-      xit 'has 16 ounces' do
+      it 'has 16 ounces' do
+        expect(preferred_drink).to be_full
       end
     end
   end
@@ -106,18 +109,21 @@ describe Drink do
     context 'when drink has 16 ounces or more' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type.
+      subject(:excessive_drink) { described_class.new(type='coffee', ounces=17) }
 
       # remove the 'x' before running this test
-      xit 'is full' do
+      it 'is full' do
+        expect(excessive_drink).to be_full
       end
     end
 
     context 'when drink has less than 16 ounces' do
       # Create an explicit subject, using 'described_class' and your choice of
       # beverage type. In addition, specify ounces to be any number under 16.
-
+      subject(:minimal_drink) { described_class.new(type='coffee', ounces=7) }
       # remove the 'x' before running this test
-      xit 'is not full' do
+      it 'is not full' do
+        expect(minimal_drink).not_to be_full
       end
     end
   end
